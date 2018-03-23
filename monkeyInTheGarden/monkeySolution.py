c67b672e26f978a07ffec9e76a5a6ca40848a1e0
AYadav01 <AYadav01@student.coppin.edu>
2018-03-23 17:11:39 -0400

    first attempt at monkeyInTheGarden challenge

--- monkeyInTheGarden/monkeySolution.py
+++ monkeyInTheGarden/monkeySolution.py
@@ -2,43 +2,47 @@ def monkeyTime(array):
 
 	#empty dictionary
 	data = {}
+
+	#this array will handle the circular references
 	newArray = list(array)
 
-	#threshold
+	#threshold point
 	divisor = (len(array)-1)//2
-	#print(divisor)
 
 	for i in range(len(array)):
 
+		#the threshold point (divisor) will increase each time the monkey chanegs tree
 		if i > 0:
 			divisor += 1
 			temp = newArray[0]
 			del(newArray[0])
 			newArray.append(temp)
-			print("i is {0}, divisor is {2} and newArray is {1}".format(i, newArray, divisor))
+			#print("i is {0}, divisor is {2} and newArray is {1}".format(i, newArray, divisor))
 
 		for j in range(len(array)):
-
 			if i != j:
 
 				if (i,j) and (j,i) not in data.keys():
-
+					#if second monkey with-in the threshold
 					if j <= divisor:
 						distance = j-i
-						
+					
+					#if second monkey is outside the threshold; then
 					elif j > divisor:
-						#print("j is {}".format(j))
-
 						target = newArray.index(array[j])
 						distance = 0
 						for indexAgain in newArray[target:]:
 							distance += 1
 
+					#finallly calculating the total cost
 					cost = array[i] + distance + array[j]
+					#writing the data to our hash table/dictionray in python ;-)
 					data[i,j] = distance, cost
 
+	#call for the findTime function
 	return findTime(data)
 
+#this function finds the highest time taken in the hashTable
 def findTime(data):
 	maximum = 0
 
